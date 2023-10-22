0c94d82dd9ed94374d17a6fdb3044270aa6857b3
Mugundan Kottur Suresh <mugundan.kottur@gmail.com>
2023-10-22 19:40:59 +0530

    Works for my use due to workaround. But still unable to open files using file Picker on external storage devices

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:open_file_plus/open_file_plus.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'dart:io' as io;
 
-void main() {
+void main() async {
   runApp(const MyApp());
 }
 
@@ -13,8 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'RFO',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.purple, scaffoldBackgroundColor: Colors.white),
       home: const MyHomePage(title: 'Flutter Random File Opener'),
     );
   }
@@ -30,16 +32,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String randomDirectory = "";
+  String fileName = "";
+  List files = [];
+  defaultLocations? _selectedLocation;
 
-  void _incrementCounter() {
+  void chooseDirectory() async {
+    String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+
     setState(() {
-      _counter++;
+      randomDirectory = selectedDirectory!;
+      files = io.Directory(selectedDirectory).listSync();
+      fileName = randomDirectory;
     });
+  }
+
+  void openRandomFile() {
+    setState(() {
+      fileName = files.length.toString();
+    });
+    if (files.isNotEmpty) {
+      int randomIndex = DateTime.now().millisecondsSinceEpoch % files.length;
+      io.File file = files[randomIndex];
+      OpenFile.open(file.path);
+      setState(() {
+        fileName = file.path;
+      });
+    }
+    else {
+      String path = "/mnt/media_rw/F251-6C3E/${_selectedLocation!.path}";
+      files = io.Directory(path).listSync();
+      int randomIndex = DateTime.now().millisecondsSinceEpoch % files.length;
+      io.File file = files[randomIndex];
+      OpenFile.open(file.path);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final pathController = TextEditingController();
+
+    final List<DropdownMenuEntry<defaultLocations>> pathEntries =
+        <DropdownMenuEntry<defaultLocations>>[];
+    for (final defaultLocations color in defaultLocations.values) {
+      pathEntries.add(
+        DropdownMenuEntry<defaultLocations>(value: color, label: color.path),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -48,21 +88,45 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            DropdownMenu<defaultLocations>(
+              initialSelection: defaultLocations.real,
+              controller: pathController,
+              label: const Text("Default Location"),
+              dropdownMenuEntries: pathEntries,
+              onSelected: (defaultLocations? value) {
+                setState(() {
+                  if(value != null) {
+                    _selectedLocation = value;
+                  }
+                });
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const SizedBox(height: 20),
+            ElevatedButton.icon(
+                onPressed: chooseDirectory,
+                icon: const Icon(Icons.folder_open, size: 48),
+                label: const Text("Choose Directory",
+                    style: TextStyle(fontSize: 14)),
+                style: ElevatedButton.styleFrom(
+                  maximumSize: const Size(150, 100),
+                )),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: openRandomFile,
+        tooltip: 'Choose Random File',
+        child: const Icon(Icons.remove_red_eye),
+      ),
     );
   }
+}
+
+enum defaultLocations {
+  real('Real', 'Real'),
+  img('Imaginary', 'Temp');
+
+  const defaultLocations(this.name, this.path);
+  final String name;
+  final String path;
 }
